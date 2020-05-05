5536e4310a24965bd2284d89ffc47fc51cdb95c6
Guido Schillaci <guido.schillaci@santannapisa.it>
2020-05-05 11:42:48 +0200

    script

--- scripts/start_docker_yarpserver.sh
+++ scripts/start_docker_yarpserver.sh
@@ -1,5 +1,24 @@
 xhost +local:root
 
+ipclient=192.168.1.229
+
+# get local ip address
+function iplocal () {
+#  ip route get 8.8.8.8 | head -1 | cut -d' ' -f8
+  ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'
+}
+# get external ip address
+function ipexternal () {
+  curl --silent http://checkip.amazonaws.com        # or:  http://ipinfo.io/ip
+}
+function ipinfo () {
+  echo "local    IP  =>  $(iplocal)"
+  echo "external IP  =>  $(ipexternal)"
+}
+
+
+ipinfo
+
 export DOCKER_CONTAINER_NAME=icub_container
 if [ ! "$(docker ps -q -f name=${DOCKER_CONTAINER_NAME})" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=${DOCKER_CONTAINER_NAME})" ]; then
@@ -10,12 +29,15 @@ if [ ! "$(docker ps -q -f name=${DOCKER_CONTAINER_NAME})" ]; then
     # run your container 
     docker run -it --rm \
       --name "$DOCKER_CONTAINER_NAME" \
-      -e DISPLAY \
+      -e DISPLAY=$ipclient:0 \
+      --env="NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all}" \
+      --env="NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics" \
+     -p10000:10000 \
       --runtime=nvidia \
       --gpus all \
       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
       guidoski/icub:tf1.12.3-gpu-py3 bash -c "
-             yarpserver --ip 172.19.0.2 --socket 10000
+             yarpserver --ip 172.19.0.2 --socket 10000 & glxgears
              "
 #--volume="/home/guido/code:/code/" \
 #&
